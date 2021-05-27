ed1381b101f0e00ad5e16082d39e8a189bf352cf
Adelar Junior <adelarjr.s@gmail.com>
2021-05-27 11:57:09 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = 'John Doe';
+    const showName = true;
+    // Estrutura condicional: {showName && name} SE showName TRUE então NAME | Também possível: {showName ?(then) name :(else) full}
+    return (
+      <div className="App">
+        <h1>Hello {showName && name}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;

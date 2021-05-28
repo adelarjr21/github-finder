14f00bec6a7b2d5391096a445d5821199d4873c7
Adelar Junior <adelarjr.s@gmail.com>
2021-05-28 12:41:49 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import UserItem from './components/users/UserItem'
 import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const showName = true;
-    // Estrutura condicional: {showName && name} SE showName TRUE então NAME | Também possível: {showName ?(then) name :(else) full}
     return (
-      <div className="App">
-        <h1>Hello {showName && name}</h1>
-      </div>
+      <nav className='App'>
+        <Navbar />
+        <UserItem />
+      </nav>
     );
   }
 }
